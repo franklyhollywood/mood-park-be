68144793eec34cb1f4e3d36be6cc93b853df0d3c
yosefkirkcohen <ycohen45@yahoo.com>
2021-10-20 13:56:34 -0700

    comments

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -10,7 +10,8 @@ async function run() {
 
     await client.query(`
             DROP TABLE IF EXISTS users CASCADE;
-            DROP TABLE IF EXISTS favorites;
+            DROP TABLE IF EXISTS favorites CASCADE;
+            DROP TABLE IF EXISTS comments;
         `);
 
     console.log(' drop tables complete', getEmoji(), getEmoji(), getEmoji());
